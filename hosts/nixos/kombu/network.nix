78fb93696ca7ff8fec845aa155eaeb0fe3762cf1
Conroy Cheers <conroy@dromeda.com.au>
2024-09-18 16:22:09 +1000

    use networkmanager

--- hosts/nixos/kombu/network.nix
+++ hosts/nixos/kombu/network.nix
@@ -11,7 +11,7 @@
     rekeyFile = "${inputs.self}/secrets/home/wifi/env.age";
   };
   networking.wireless = {
-    enable = true;
+    # enable = true;
     userControlled.enable = true;
     environmentFile = config.age.secrets."home.wifi.env".path;
     networks = {
@@ -30,28 +30,30 @@
   };
   networking.useNetworkd = false;
 
-  systemd.network.enable = false;
-  systemd.network.networks = {
-    "10-enp5s0" = {
-      DHCP = "yes";
-      matchConfig.MACAddress = "00:e0:1f:bd:81:cb";
-      networkConfig = {
-        IPv6PrivacyExtensions = "yes";
-        MulticastDNS = true;
-      };
-      dhcpV4Config.RouteMetric = 10;
-      dhcpV6Config.RouteMetric = 10;
-    };
+  networking.networkmanager.enable = true;
 
-    "15-wlan0" = {
-      DHCP = "yes";
-      matchConfig.MACAddress = "6c:2f:80:e0:ce:2d";
-      networkConfig = {
-        IPv6PrivacyExtensions = "yes";
-        MulticastDNS = true;
-      };
-    };
-  };
+  # systemd.network.enable = false;
+  # systemd.network.networks = {
+  #   "10-enp5s0" = {
+  #     DHCP = "yes";
+  #     matchConfig.MACAddress = "00:e0:1f:bd:81:cb";
+  #     networkConfig = {
+  #       IPv6PrivacyExtensions = "yes";
+  #       MulticastDNS = true;
+  #     };
+  #     dhcpV4Config.RouteMetric = 10;
+  #     dhcpV6Config.RouteMetric = 10;
+  #   };
+
+  #   "15-wlan0" = {
+  #     DHCP = "yes";
+  #     matchConfig.MACAddress = "6c:2f:80:e0:ce:2d";
+  #     networkConfig = {
+  #       IPv6PrivacyExtensions = "yes";
+  #       MulticastDNS = true;
+  #     };
+  #   };
+  # };
 
   # systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
 }
