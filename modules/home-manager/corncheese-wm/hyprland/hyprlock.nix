57d494c72251984f4dfc69452c33b00b5976fcd8
Conroy Cheers <conroy@dromeda.com.au>
2025-04-14 16:44:56 +1000

    wm: add hypridle

--- modules/home-manager/corncheese-wm/hyprland/hyprlock.nix
+++ modules/home-manager/corncheese-wm/hyprland/hyprlock.nix
@@ -5,6 +5,29 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "hyprlock";
+        };
+
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
+
     programs.hyprlock.enable = true;
     programs.hyprlock.settings = {
       general = {
